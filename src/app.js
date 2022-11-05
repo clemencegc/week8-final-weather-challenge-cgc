67f419c705e03f7ec54cd70a0829145a36edf72d
clemencegc <clemence.giraudch@gmail.com>
2022-11-05 10:49:46 +0100

    Removed console.log

--- src/app.js
+++ src/app.js
@@ -69,7 +69,6 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = "34ae1065362d42545661451bda2b8a1f";
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKkey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);

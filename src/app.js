aab25626592fdbb66549af40a51b06e3cb50c2cc
clemencegc <clemence.giraudch@gmail.com>
2022-11-06 13:10:15 +0100

    Css and fix error axios

--- src/app.js
+++ src/app.js
@@ -103,7 +103,6 @@ function search(city) {
   let apiKey = "2e5b6f97a330658a59719220e02b26a0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
-  Ò;
 }
 
 function handleSubmit(event) {
